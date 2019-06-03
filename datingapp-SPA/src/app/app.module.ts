2e682390966713a193754631b8d8c3be8993a138
Kang Rong <kangrong@Kangs-MacBook-Pro.local>
2019-06-03 12:03:44 +0930

    3/6 commit

--- datingapp-SPA/src/app/app.module.ts
+++ datingapp-SPA/src/app/app.module.ts
@@ -1,23 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+
+   ],
    bootstrap: [
       AppComponent
    ]
